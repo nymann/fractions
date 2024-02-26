3fd1b05e0cf976840c60e4b0cb70292c919696ab
Kristian Jakobsen <kristian@nymann.dev>
2024-02-26 19:15:05 +0100

    Add support for 0 numerators

--- src/main/java/dev/nymann/Fraction.java
+++ src/main/java/dev/nymann/Fraction.java
@@ -23,11 +23,14 @@ public class Fraction {
     }
 
     public Fraction plus(Fraction fraction) {
+        if (fraction.numerator == 0 && numerator == 0) {
+            return new Fraction(0, 0);
+        }
         if (fraction.denominator != denominator) {
             throw new RuntimeException("Not implemented");
         }
         var n = fraction.numerator + numerator;
-        if(denominator % n == 0) {
+        if (denominator % n == 0) {
             return new Fraction(1, denominator / n);
         }
         throw new RuntimeException("Not implemented");
--- src/test/java/TestFraction.java
+++ src/test/java/TestFraction.java
@@ -1,5 +1,4 @@
 import dev.nymann.Fraction;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,10 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestFraction {
     @Test
     public void testSimpleFraction() {
-        // 1/2 + 1/2 = 2/4 = 1/2
-        Fraction a = new Fraction(1, 4);
-        Fraction b = new Fraction(1, 4);
+        Fraction actual = new Fraction(1, 4).plus(new Fraction(1, 4));
         Fraction expected = new Fraction(1, 2);
-        assertEquals(expected, a.plus(b));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNonEqualNumerators() {
+        var actual = new Fraction(1, 8).plus(new Fraction(3, 8));
+        var expected = new Fraction(1, 2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNumerator0() {
+        var actual = new Fraction(0, 1).plus(new Fraction(0, 2));
+        var expected = new Fraction(0, 0);
+        assertEquals(expected, actual);
     }
 }
